88eab470061176eb53e4571cd03bd8eaf924d665
Роман Сингур <zoo1102@mail.ru>
2023-12-07 21:41:30 +0500

    Задание 1. Обработчик строк

--- Maxim/Program.cs
+++ Maxim/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var stringHandler = new StringHandler(Console.ReadLine());
+var doneString = stringHandler.Handling(); 
+Console.WriteLine(doneString);
