63d0b3eaef85bd423a7206f198e4e83349916fe9
Роман Сингур <zoo1102@mail.ru>
2023-12-08 00:12:37 +0500

    Задание 4. Начало работы над поиском подстроки

--- Maxim/Program.cs
+++ Maxim/Program.cs
@@ -1,5 +1,30 @@
 ﻿using Maxim;
 
+static string SubstringSerching(string inputStr)
+{
+    string vowels = "aeiouy";
+    int fistVowel = -1;
+    int lastVowel = -1;
+    for (var i = 0; i < inputStr.Length; i++)
+    {
+        if (vowels.Contains(inputStr[i]))
+        {
+            if (fistVowel == -1)
+            {
+                fistVowel = i;
+            }
+            lastVowel = i;
+        }
+
+        if (fistVowel != -1)
+        {
+            return inputStr.Substring(fistVowel, lastVowel-fistVowel+1);
+        }
+    }
+
+    return "";
+}
+
 var stringHandler = new StringHandler(Console.ReadLine());
 
 if (stringHandler.CheckString())
@@ -9,4 +34,5 @@ if (stringHandler.CheckString())
     var charCounter = new CharCouter(doneString);
     charCounter.CountChar();
     charCounter.GetCount();
+    Console.WriteLine(SubstringSerching(doneString));
 }    
